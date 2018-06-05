25b9faa17b5ac9aacbf357a69fdcd60c61cd96f2
Olga Kononova <olga@landau.lbl.gov>
2018-06-05 12:05:06 -0700

    Updated MaterialParser:  - Corrected files pathways  - Optional search in PubChem  - Using pre-existing dictionary  - Fixed bugs in get_dopants() method

--- material_parser/example.py
+++ material_parser/example.py
@@ -1,27 +1,26 @@
-from synthesis_project_ceder.nlp.preprocessing import TextPreprocessor
+# coding=utf-8
 from material_parser import MaterialParser
 import pprint
 from chemdataextractor.doc import Paragraph
 
 
 def find_variables(var, text):
-    sents = Paragraph(tp._preprocess_text(text)).sentences
+    sents = Paragraph(text).sentences
     i = 0
     sent = sents[i]
     values = []
+    mode = ''
     try:
         while len(values) == 0 and i < len(sents):
             sent = sents[i]
-            values = mp.get_stoichiometric_values(var, sent.text)
+            values, mode = mp.get_stoichiometric_values(var, sent.text)
             i = i + 1
     except:
         print('Failed to extract ' + var + ' from text:')
-        print(tp._preprocess_text(text))
 
     return values
 
 
-tp = TextPreprocessor('')
 mp = MaterialParser()
 
 list_of_targets = ['(1-x)Na0.5Sm0.5TiO3-xSm(Mg0.5Ti0.5)O3', 'NST-SMTx', 'Na0.5Sm0.5TiO3',
@@ -31,23 +30,24 @@ paragraphs = [
     solid solutions were investigated. The results showed that, after being calcined at 1100 °C for 2 h, the proposed \
     NST system sintered at 1350 °C for 4 h exhibited a good microwave dielectric properties. \
     Additionally, the phase assemblages, crystal structures, microstructures and microwave dielectric properties of \
-    the Na0.5Sm0.5[Ti1−y(Al0.5Ta0.5)y]O3 (NSTATx, 0.1 ≤ y ≤ 0.3) and (1 − x)Na0.5Sm0.5TiO3–xSm(Mg0.5Ti0.5)O3 \
-    (NST–SMTx, 0.1 ≤ x ≤ 0.4) ceramics were also investigated in this work. \
+    the Na0.5Sm0.5[Ti1-y(Al0.5Ta0.5)y]O3 (NSTATx, 0.1 ≤ y ≤ 0.3) and (1 - x)Na0.5Sm0.5TiO3-xSm(Mg0.5Ti0.5)O3 \
+    (NST-SMTx, 0.1 ≤ x ≤ 0.4) ceramics were also investigated in this work. \
     The X-ray diffractometer results revealed that a tilted orthorhombic perovskite structure in space group Pnma \
     was refined in the NSTATx ceramics, while the Ti7O13 phase, Sm2Ti2O7 phase, and some unknown phase were detected \
-    gradually in NST–SMTx ceramics with increasing x value.',
-    'Na0.5Sm0.5TiO3 (NST), Na0.5Sm0.5[Ti1−x(Al0.5Ta0.5)x]O3 (NSTATx) and (1 − x)Na0.5Sm0.5TiO3–xSm(Mg0.5Ti0.5)O3 \
-    (NST–SMTx) specimens were synthesized by a conventional mixed-oxide route using high purity (≥99.5 %) oxide powders \
+    gradually in NST-SMTx ceramics with increasing x value.',
+    'Na0.5Sm0.5TiO3 (NST), Na0.5Sm0.5[Ti1-x(Al0.5Ta0.5)x]O3 (NSTATx) and (1 - x)Na0.5Sm0.5TiO3-xSm(Mg0.5Ti0.5)O3 \
+    (NST-SMTx) specimens were synthesized by a conventional mixed-oxide route using high purity (≥99.5 %) oxide powders \
     of Sm2O3, Al2O3, Ta2O5, MgO, Na2CO3, and TiO2. \
     The starting NST powders were weighed and mixed with ZrO2 balls in ethanol for 24 h, then dried, sieved through \
-    100-mesh screen, and calcined at 900–1100 °C for 2 h in alumina crucible. \
+    100-mesh screen, and calcined at 900-1100 °C for 2 h in alumina crucible. \
     Additionally, the synthesized powders were weighed according to stoichiometric ratio of \
-    NSTATx (y = 0.1, 0.2 and 0.3) and NST–SMTx (x = 0.1, 0.2, 0.3 and 0.4), and then ground in ethanol for 24 h with \
+    NSTATx (y = 0.1, 0.2 and 0.3) and NST-SMTx (x = 0.1, 0.2, 0.3 and 0.4), and then ground in ethanol for 24 h with \
     ZrO2 balls. Mixtures were dried, sieved and calcined at 1100 °C for 2 h. \
     All the calcined powders were ground and mixed with 5 wt% PVA poly(vinyl alcohol) as binder. \
     The granulated powders were uni-axially pressed into pellets with 11.5 mm in diameter and 1.5–6 mm in height under \
     the pressure of 180 MPa. After being fired to remove an organic binder, these pellets were sintered at \
-    1300–1520 °C for different times in air.']
+    1300-1520 °C for different times in air.']
+
 
 print(list_of_targets)
 print(paragraphs)
@@ -66,8 +66,13 @@ for material in list_of_targets:
     data['targets_upd'].append(new_material)
 
 # Resolve abbreviations:
-data['abbreviations'] = mp.get_abbreviations_dict(data['targets_upd'], data['paragraphs'])
-data['targets_upd_2'] = mp.substitute_abbreviations(data['targets_upd'], data['abbreviations'])
+data['abbreviations'] = mp.build_abbreviations_dict(data['targets_upd'], data['paragraphs'])
+data['targets_upd_2'] = []
+for material in data['targets_upd']:
+    if material in data['abbreviations']:
+        data['targets_upd_2'].append(data['abbreviations'][material])
+    else:
+        data['targets_upd_2'].append(material)
 
 print('Initial targets:', data['targets'])
 print('After removing dopants:', data['targets_upd'])
@@ -84,7 +89,6 @@ for target in data['targets_upd_2']:
     try:
         t_struct = mp.get_chemical_structure(target)
         data['chemical_structure'][target] = t_struct
-        pprint.pprint(t_struct)
     except:
         print('Failed!')
 
@@ -99,7 +103,10 @@ for name, struct in data['chemical_structure'].items():
             print("Found variable: "+var)
             print("Found values: "+str(values))
 
+pprint.pprint(data['chemical_structure'])
+
 print('-----------------------------------------------')
+
 
 
 
